664f32f58c34d26b30b09c77bcb4e891b41a3054
Soham <sohamnathani678@gmail.com>
2018-09-30 16:38:38 +0530

    abou page ready final

--- about.php
+++ about.php
@@ -7,23 +7,23 @@
     <link rel="stylesheet" type="text/css" href="assets/css/about.css">
     <link rel="stylesheet" type="text/css" href="assets/css/footer.css">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link href="https://fonts.googleapis.com/css?family=Open+Sans:400,700" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans:400,700,600,500" rel="stylesheet">
     <link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
 <body>
     <header>
 			<?php include "Includes/nav-bar.php" ?>
 </header>
-    <main class="row">
-        <div class="col-1"></div>
-        <div class="col-10 container">
+    <main>
+        
+        <div class="container">
             <div class="objective">
-                <h1>Objective</h1>
+                <h1>- Our Mission -</h1>
                 <p>Lorem Ipsum Dolor Amet</p>`
             </div>
             <div class="committee">
-                <h1>Committee</h1>
-                <div>
+                <h1>- Committee -</h1>
+                <div class="table_m">
                     <table>
                         <tr>
                             <th>No.</th>
@@ -31,12 +31,42 @@
                             <th>Post</th>
                             <th>Mobile No.</th>
                         </tr>
+                        <tr>
+                        	<td>1</td>
+                        	<td>Mr. Nagendra Singh "Judev"</td>
+                        	<td>Sanrakshak</td>
+                        	<td>9013869227</td>
+                        </tr>
+                        <tr>
+                        	<td>2</td>
+                        	<td>Mr. Nagendra Singh "Judev"</td>
+                        	<td>Sanrakshak</td>
+                        	<td>9013869227</td>
+                        </tr>
+                        <tr>
+                        	<td>3</td>
+                        	<td>Mr. Nagendra Singh "Judev"</td>
+                        	<td>Sanrakshak</td>
+                        	<td>9013869227</td>
+                        </tr>
+                        <tr>
+                        	<td>4</td>
+                        	<td>Mr. Nagendra Singh "Judev"</td>
+                        	<td>Sanrakshak</td>
+                        	<td>9013869227</td>
+                        </tr>
+                        <tr>
+                        	<td>5</td>
+                        	<td>Mr. Nagendra Singh "Judev"</td>
+                        	<td>Sanrakshak</td>
+                        	<td>9013869227</td>
+                        </tr>
                     </table>
                 </div>
             </div>
             <div class="faculty">
-                <h1>Faculty</h1>
-                <div>
+                <h1>- Faculty -</h1>
+                <div class="table_m">
                     <table>
                         <tr>
                             <th>No.</th>
@@ -45,26 +75,57 @@
                             
 
                         </tr>
+                        <tr>
+                        	<td>1</td>
+                        	<td>Mr. Nagendra Singh "Judev"</td>
+                        	<td>Sanrakshak</td>
+                        </tr>
+                        <tr>
+                        	<td>2</td>
+                        	<td>Mr. Nagendra Singh "Judev"</td>
+                        	<td>Sanrakshak</td>
+                        </tr>
+                        <tr>
+                        	<td>3</td>
+                        	<td>Mr. Nagendra Singh "Judev"</td>
+                        	<td>Sanrakshak</td>
+                        </tr>
+                        <tr>
+                        	<td>4</td>
+                        	<td>Mr. Nagendra Singh "Judev"</td>
+                        	<td>Sanrakshak</td>
+                        </tr>
                     </table>
                 </div>
             </div>
-            <div class="infra_s">
-                <h1>Infrastructure</h1>
-                  
-                <div class="infra_d">
-                  <img src="assets/Images/random2.jpg">
-                  <p>This is the beach and lorem ipsum dolor amet</p>
-                </div>
-                <div class="infra_d">
-                  <img src="assets/Images/random2.jpg">
-                  <p>This is the beach and lorem ipsum dolor amet</p>
-                </div>
+             <div class="infra_s">
+                <h1>- Infrastructure -</h1>
+	                  
+	                <div class="infra_d">
+	                  <img src="assets/Images/random2.jpg">
+	                  <p>This is the beach and lorem ipsum dolor amet</p>
+	                </div>
+	                <div class="infra_d">
+	                  <img src="assets/Images/random2.jpg">
+	                  <p>This is the beach and lorem ipsum dolor amet</p>
+	                </div>
+	                <div class="infra_d">
+	                  <img src="assets/Images/random2.jpg">
+	                  <p>This is the beach and lorem ipsum dolor amet</p>
+	                </div>
+	                <div class="infra_d">
+	                  <img src="assets/Images/random2.jpg">
+	                  <p>This is the beach and lorem ipsum dolor amet</p>
+	                </div>
             </div>        
+        </div>
+    </main>
             <script type="text/javascript" src="../assets/js/page.js"></script>
         </body>
         <footer>
             <?php include_once "Includes/footer.php" ?>
         </footer>
+
         </html>
 <!-- #406e9d opaci
 opacity 36</span>.%
