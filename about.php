c936fb10112670084ecc1a704f98c5e62cee6cfe
Anand Sharma <anandsharma110397@gmail.com>
2018-09-30 20:00:11 +0530

    sudhaar

--- about.php
+++ about.php
@@ -6,25 +6,24 @@
     <link rel="stylesheet" type="text/css" href="assets/css/s-g.css">
     <link rel="stylesheet" type="text/css" href="assets/css/nav-bar.css">
     <link rel="stylesheet" type="text/css" href="assets/css/about.css">
-    <link rel="stylesheet" type="text/css" href="assets/css/footer.css">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link href="https://fonts.googleapis.com/css?family=Open+Sans:400,700" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans:400,700,600,500" rel="stylesheet">
     <link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
 <body>
     <header>
 			<?php include "Includes/nav-bar.php" ?>
 </header>
-    <main class="row">
-        <div class="col-1"></div>
-        <div class="col-10 container">
+    <main>
+        
+        <div class="container">
             <div class="objective">
-                <h1>Objective</h1>
+                <h1>- Our Mission -</h1>
                 <p>Lorem Ipsum Dolor Amet</p>`
             </div>
             <div class="committee">
-                <h1>Committee</h1>
-                <div>
+                <h1>- Committee -</h1>
+                <div class="table_m">
                     <table>
                         <tr>
                             <th>No.</th>
@@ -32,12 +31,42 @@
                             <th>Post</th>
                             <th>Mobile No.</th>
                         </tr>
+                        <tr>
+                        	<td>1</td>
+                        	<td>Mr. Nagendra Singh "Judev"</td>
+                        	<td>Sanrakshak</td>
+                        	<td>9013869227</td>
+                        </tr>
+                        <tr>
+                        	<td>2</td>
+                        	<td>Mr. Nagendra Singh "Judev"</td>
+                        	<td>Sanrakshak</td>
+                        	<td>9013869227</td>
+                        </tr>
+                        <tr>
+                        	<td>3</td>
+                        	<td>Mr. Nagendra Singh "Judev"</td>
+                        	<td>Sanrakshak</td>
+                        	<td>9013869227</td>
+                        </tr>
+                        <tr>
+                        	<td>4</td>
+                        	<td>Mr. Nagendra Singh "Judev"</td>
+                        	<td>Sanrakshak</td>
+                        	<td>9013869227</td>
+                        </tr>
+                        <tr>
+                        	<td>5</td>
+                        	<td>Mr. Nagendra Singh "Judev"</td>
+                        	<td>Sanrakshak</td>
+                        	<td>9013869227</td>
+                        </tr>
                     </table>
                 </div>
             </div>
             <div class="faculty">
-                <h1>Faculty</h1>
-                <div>
+                <h1>- Faculty -</h1>
+                <div class="table_m">
                     <table>
                         <tr>
                             <th>No.</th>
@@ -46,27 +75,72 @@
                             
 
                         </tr>
+                        <tr>
+                        	<td>1</td>
+                        	<td>Mr. Nagendra Singh "Judev"</td>
+                        	<td>Sanrakshak</td>
+                        </tr>
+                        <tr>
+                        	<td>2</td>
+                        	<td>Mr. Nagendra Singh "Judev"</td>
+                        	<td>Sanrakshak</td>
+                        </tr>
+                        <tr>
+                        	<td>3</td>
+                        	<td>Mr. Nagendra Singh "Judev"</td>
+                        	<td>Sanrakshak</td>
+                        </tr>
+                        <tr>
+                        	<td>4</td>
+                        	<td>Mr. Nagendra Singh "Judev"</td>
+                        	<td>Sanrakshak</td>
+                        </tr>
                     </table>
                 </div>
             </div>
-            <div class="infra_s">
-                <h1>Infrastructure</h1>
-                  
-                <div class="infra_d">
-                  <img src="assets/Images/random2.jpg">
-                  <p>This is the beach and lorem ipsum dolor amet</p>
-                </div>
-                <div class="infra_d">
-                  <img src="assets/Images/random2.jpg">
-                  <p>This is the beach and lorem ipsum dolor amet</p>
-                </div>
+             <div class="infra_s">
+                <h1>- Infrastructure -</h1>
+	                  
+	                <div class="infra_d">
+	                  <img src="assets/Images/random2.jpg">
+	                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vitae urna dignissim,
+                       vehicula odio at, dictum libero. Praesent ac dapibus odio. Nullam ut justo at dui 
+                       egestas elementum et quis nisl. Praesent molestie nibh eget facilisis posuere. 
+                       Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae
+                       </p>
+	                </div>
+	                <div class="infra_d">
+	                  <img src="assets/Images/random2.jpg">
+	                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vitae urna dignissim,
+                       vehicula odio at, dictum libero. Praesent ac dapibus odio. Nullam ut justo at dui 
+                       egestas elementum et quis nisl. Praesent molestie nibh eget facilisis posuere. 
+                       Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae
+                       </p>
+	                </div>
+	                <div class="infra_d">
+	                  <img src="assets/Images/random2.jpg">
+	                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vitae urna dignissim,
+                       vehicula odio at, dictum libero. Praesent ac dapibus odio. Nullam ut justo at dui 
+                       egestas elementum et quis nisl. Praesent molestie nibh eget facilisis posuere. 
+                       Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae
+                       </p>
+	                </div>
+	                <div class="infra_d">
+	                  <img src="assets/Images/random2.jpg">
+	                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vitae urna dignissim,
+                       vehicula odio at, dictum libero. Praesent ac dapibus odio. Nullam ut justo at dui 
+                       egestas elementum et quis nisl. Praesent molestie nibh eget facilisis posuere. 
+                       Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae
+                       </p>
+	                </div>
             </div>        
+        </div>
+            <?php include_once "Includes/footer.php" ?>
+
+    </main>
             <script type="text/javascript" src="../assets/js/page.js"></script>
         </body>
-        <footer>
-            <?php include_once "Includes/footer.php" ?>
-        </footer>
-        </html>
+</html>
 <!-- #406e9d opaci
 opacity 36</span>.%
  -->
