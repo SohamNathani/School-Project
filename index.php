6e6ed3f45a6b7ec7bec96d5ae90f322803fe01b1
Anand Sharma <anandsharma110397@gmail.com>
2018-10-01 13:16:54 +0530

    added contact page

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
     <link rel="stylesheet" href="assets/css/s-g.css">
     <link rel="stylesheet" href="assets/css/main.css">
     <link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-    <title>Home</title>
+    <title>Saraswati Vidya Mandir, Nagod</title>
 </head>
 
 <body>
@@ -19,93 +19,112 @@
     </header>
     <main>
         <!-- Main Row of the Page -->
-     <div class="row">
-         <!-- Column with taking as the whole page cover -->
+        <div class="row">
+            <!-- Column with taking as the whole page cover -->
             <div class="col-12 p-0">
-            <!-- First Row of the page with school-image in it -->
-            <div class="row">
-                <!-- School-Image With quote inserted in it -->
-            <div class="school-image">
-        <img src="https://jooinn.com/images/school-building-3.jpg" class="S-image" alt="School-Image" width="600" height="400">
-        <div class="middle">
-        <div class="s-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam blandit sem et lectus aliquam, in varius
-             lectus interdum. Quisque ultrices.</div>
-        </div>
-    </div>
-        </div>
-            <!-- First Row ends here -->
-            <!-- NEWS SECTION ROW -->
-
-
-
-
-
-
-
-
-
-
-
-
-
-            <!-- News SECTION END HERE -->
-            <!-- Second Row begins here with Speciality of the school-->
-            <div class="row l-div Speciality">
-                <div class="col-1"></div>
-                <div class="col-10">
-                        <h1 style="">Our Speciality</h1>
-
-                <div class="col-6">
-                    <ul class="a-eo">
-                        <li>Lorem Ipsum</li>
-                        <li>Lorem Ipsum</li>
-                        <li>Lorem Ipsum</li>
-                        <li>Lorem Ipsum</li>
-                        <li>Lorem Ipsum</li>
-                        <li>Lorem Ipsum</li>
-                        <li>Lorem Ipsum</li>
-                        <li>Lorem Ipsum</li>
-                        <li>Lorem Ipsum</li>
-                    </ul>
+                <!-- First Row of the page with school-image in it -->
+                <div class="row">
+                    <!-- School-Image With quote inserted in it -->
+                    <div class="school-image">
+                        <img src="https://jooinn.com/images/school-building-3.jpg" class="S-image" alt="School-Image"
+                            width="600" height="400">
+                        <div class="middle">
+                            <div class="s-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam blandit
+                                sem et lectus aliquam, in varius
+                                lectus interdum. Quisque ultrices.</div>
+                        </div>
+                    </div>
                 </div>
-                <div class="col-6">
-                    <ul class="a-eo">
-                            <li>Lorem Ipsum</li>
-                            <li>Lorem Ipsum</li>
-                            <li>Lorem Ipsum</li>
-                            <li>Lorem Ipsum</li>
-                            <li>Lorem Ipsum</li>
-                            <li>Lorem Ipsum</li>
-                            <li>Lorem Ipsum</li>
-                            <li>Lorem Ipsum</li>
-                            <li>Lorem Ipsum</li>
-                    </ul>
+                <!-- First Row ends here -->
+                <!-- NEWS SECTION ROW -->
+                
+
+                    <div class="row">
+                        <div class="col-1"></div>
+                        <div class="col-10 s-news">
+                            <h1>NEWS</h1>
+                            <ul>
+                                <li class="news-item"><a class="news-link" href="http://" target="_blank" rel="noopener noreferrer">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut id interdum purus, sit amet pellentesque.</a></li>
+                                <li class="news-item"><a class="news-link" href="http://" target="_blank" rel="noopener noreferrer">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut id interdum purus, sit amet pellentesque.</a></li>
+                                <li class="news-item"><a class="news-link" href="http://" target="_blank" rel="noopener noreferrer">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut id interdum purus, sit amet pellentesque.</a></li>
+                                <li class="news-item"><a class="news-link" href="http://" target="_blank" rel="noopener noreferrer">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut id interdum purus, sit amet pellentesque.</a></li>
+                                <li class="news-item"><a class="news-link n-disable" href="http://" target="_blank" rel="noopener noreferrer">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut id interdum purus, sit amet pellentesque.</a></li>
+                            </ul>
+                        </div>
+                        <div class="col-1"></div>
+                    </div>
+
+
+                <!-- News SECTION END HERE -->
+                <!-- Second Row begins here with Speciality of the school-->
+                <div class="row l-div Speciality">
+                    <div class="col-1"></div>
+                    <div class="col-10">
+                        <h1>Our Speciality</h1>
+                            <!-- 
+                            Speciality List items
+                             -->
+                        <div class="col-6">
+                            <ul class="a-eo">
+                                <li>Lorem Ipsum</li>
+                                <li>Lorem Ipsum</li>
+                                <li>Lorem Ipsum</li>
+                                <li>Lorem Ipsum</li>
+                                <li>Lorem Ipsum</li>
+                                <li>Lorem Ipsum</li>
+                                <li>Lorem Ipsum</li>
+                                <li>Lorem Ipsum</li>
+                                <li>Lorem Ipsum</li>
+                            </ul>
+                        </div>
+                        <div class="col-6">
+                            <ul class="a-eo">
+                                <li>Lorem Ipsum</li>
+                                <li>Lorem Ipsum</li>
+                                <li>Lorem Ipsum</li>
+                                <li>Lorem Ipsum</li>
+                                <li>Lorem Ipsum</li>
+                                <li>Lorem Ipsum</li>
+                                <li>Lorem Ipsum</li>
+                                <li>Lorem Ipsum</li>
+                                <li>Lorem Ipsum</li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col-1"></div>
                 </div>
-                </div>
-                <div class="col-1"></div>
-            </div>
-            <!-- Second Row ends here -->
-            <!-- Third Row of the page begins here with Facilities -->
-            <div class="row l-div facility">
-                <div class="col-1"></div>
-                <div class="col-10">
+                <!-- Second Row ends here -->
+                <!-- Third Row of the page begins here with Facilities -->
+                <div class="row l-div facility">
+                    <div class="col-1"></div>
+                    <div class="col-10">
                         <h1>Facilities</h1>
-                <div class="col-4 Over-lay">
-                    <img class="lab-img" src="http://www.dobbsschool.com/images/blog_img4.jpg" alt="">
-                    <div><div class="text">Science Laboratory</div></div></div>
-                <div class="col-4 Over-lay">
-                    <img class="lab-img" src="http://www.udgamschool.com/UnzipImages/Computer%20Laboratory/12.JPG" alt="">
-                    <div><div class="text">Computer Laboratory</div></div></div>
-                <div class="col-4 Over-lay">
-                    <img class="lab-img" src="http://davcsp.org/File/50/Mathlab.JPG" alt="">
-                   <div><div class="text">Mathematics Laboratory</div></div></div>
+                        <div class="col-4 Over-lay">
+                            <img class="lab-img" src="http://www.dobbsschool.com/images/blog_img4.jpg" alt="">
+                            <div>
+                                <div class="text">Science Laboratory</div>
+                            </div>
+                        </div>
+                        <div class="col-4 Over-lay">
+                            <img class="lab-img" src="http://www.udgamschool.com/UnzipImages/Computer%20Laboratory/12.JPG"
+                                alt="">
+                            <div>
+                                <div class="text">Computer Laboratory</div>
+                            </div>
+                        </div>
+                        <div class="col-4 Over-lay">
+                            <img class="lab-img" src="http://davcsp.org/File/50/Mathlab.JPG" alt="">
+                            <div>
+                                <div class="text">Mathematics Laboratory</div>
+                            </div>
+                        </div>
 
+                    </div>
+                    <div class="col-1"></div>
+                </div>
+                <!-- Third Row Ends here -->
             </div>
-            <div class="col-1"></div>
-            </div>
-            <!-- Third Row Ends here -->
-        </div>   
-    </div>
+        </div>
         <!-- Main Row ends here -->
     </main>
     <footer>
@@ -114,16 +133,15 @@
     </footer>
     <script type="text/javascript">
         var nav = document.getElementById("toggle");
-        function myFunction(){
-            
-        if (nav.className === "nav-bar") {
-            nav.className += " responsive";
-        } else {
-            nav.className = "nav-bar";
-        }
-        }
 
+        function myFunction() {
 
+            if (nav.className === "nav-bar") {
+                nav.className += " responsive";
+            } else {
+                nav.className = "nav-bar";
+            }
+        }
     </script>
 </body>
 
